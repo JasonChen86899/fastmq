2c84a5ac0602ccf9804bc4c4999fc505bd59f10d
Jason Chen <1538682075@qq.com>
2016-09-10 02:51:55 +0800

    Distrubtion Lock By ZK to set up FastMQ

--- src/main/java/MQ/ClusterManager.java
+++ src/main/java/MQ/ClusterManager.java
@@ -1,11 +1,9 @@
 package MQ;
 
-import com.github.zkclient.IZkChildListener;
 import com.github.zkclient.ZkClient;
 import org.apache.zookeeper.CreateMode;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.LockSupport;
 
 /**
@@ -13,21 +11,15 @@ import java.util.concurrent.locks.LockSupport;
  */
 public class ClusterManager extends Thread {
     /**
-     * 中断标志位，1未中断，0表示中断，不用Thread.isInterrupt()的原因是由于JVM会自动清除中断标识位
-     */
-    private static AtomicInteger interruptFalg = new AtomicInteger(1);
-    /**
      * 建立连接，创建持久点进行消费服务器的注册
      */
     private static ZkClient zkClient;
 
     private static int childrenNum;
 
-    private static int MQServerNodesNum;
+    //private static volatile List<String> currentChilds;
 
-    private static volatile List<String> currentChilds;
-
-    private static volatile List<String> MQServerNodes;
+    //private static volatile List<String> MQServerNodes;
 
     private static void creatRootNode(){
         String Zkservers = "127.0.0.1:8880,127.0.0.1:8881,127.0.0.1:8881";
@@ -49,45 +41,66 @@ public class ClusterManager extends Thread {
         }
     }
 
-    protected static void createMQServerNode(String data){
-        for(;;){//自旋非阻塞
-            try{
-                zkClient.createPersistent("/MQServers"+MQServerNodesNum);
-                zkClient.writeData("/MQServers"+MQServerNodesNum,data.getBytes());
-            }catch (Exception e){
-                MQServerNodesNum++;
-            }
-        }
+    protected static void createMQServerNode(String ipAddress){
+        if (!zkClient.exists("/MQServers"))
+            zkClient.createPersistent("/MQServers");
+        zkClient.createPersistent("/MQServers/"+ipAddress);
+        //zkClient.writeData("")
+        //zkClient.writeData("/MQServers"+MQServerNodesNum,data.getBytes());
     }
 
+    /**
     private static void SubscribeChildChanges(){
         IZkChildListener iZkChildListener = (String var1, List<String> var2) ->{
-            currentChilds = var2;
+            //currentChilds = var2;
         };
         zkClient.subscribeChildChanges("/Messageconsumer",iZkChildListener);
-    }
+    }**/
 
+    /**
     private static void SubscribeMQServerNodesChanges(){
         IZkChildListener iZkChildListener = (String var1, List<String> var2) -> {
-            MQServerNodes = var2;
+            //MQServerNodes = var2;
+            List<String> aliveMQServernodes = ClusterManager.getMQServerNodes();
+            //if(aliveMQServernodes.stream().filter((a) -> a.equals()).findFirst();){
+                //return;
+            //}
+
+            String ipAddress = aliveMQServernodes.get(new Random(aliveMQServernodes.size()-1).nextInt());
+            ZMQ.Context context = ZMQ.context(1);
+            ZMQ.Socket socket = context.socket(ZMQ.PUSH);
+            socket.connect(ipAddress);
+            socket.send("SetupFastMQ");
+
         };
         zkClient.subscribeChildChanges("/MQServers",iZkChildListener);
-    }
+    }*/
 
     public static List getCurrentChilds(){
-        return currentChilds;
+        return zkClient.getChildren("/Messageconsumer");
     }
 
     public static List getMQServerNodes(){
-        return MQServerNodes;
+        return zkClient.getChildren("/MQServers");
+    }
+
+    public static ZkClient getZkClient(){
+        if(zkClient != null)
+            return zkClient;
+        else {
+            String Zkservers = "127.0.0.1:8880,127.0.0.1:8881,127.0.0.1:8881";
+            zkClient = new ZkClient(Zkservers,10000,10000);
+            System.out.println("connect ok!");
+            return zkClient;
+        }
     }
 
     public void run(){
         creatRootNode();//开启zk集群
-        SubscribeMQServerNodesChanges();//注册MQ服务器集群监听事件；跟ZK集群不一样
-        SubscribeChildChanges();//注册消息服务器监听事件
+        //SubscribeMQServerNodesChanges();//注册MQ服务器集群监听事件；跟ZK集群不一样
+        //SubscribeChildChanges();//注册消息服务器监听事件
         for(;;){
-            while (interruptFalg.intValue()==1){
+            while (Thread.currentThread().isInterrupted()){
                 //这里可以跟日志接口进行对接，记录监控进程的运行情况
             }
             LockSupport.park(Thread.currentThread());//阻塞当前的线程
--- src/main/java/MQ/SetupFastMQ.java
+++ src/main/java/MQ/SetupFastMQ.java
@@ -1,7 +1,100 @@
 package MQ;
 
+import com.github.zkclient.IZkDataListener;
+import com.github.zkclient.exception.ZkNoNodeException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.zeromq.ZMQ;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
 /**
- * Created by I330347 on 2016/9/8.
+ * Created by Jason Chen on 2016/9/8.
  */
-public class SetupFastMQ {
+
+/**
+ * 每台MQServer需要启动这样一个线程，用来监听ZKClient的消息，以便PullSingleton线程消失，重新起一个线程
+ */
+public class SetupFastMQ extends Thread {
+    @Autowired
+    private MQService FastMQ;
+    private ZMQ.Context context;
+    private ZMQ.Socket setupFastMQLocalSocket;
+    private String ipAddress;
+    /**
+     * 分布式锁，用来监控每台机器的SetFastMQ，只有一台机器获得锁，然后监听前一个节点的删除事件
+     */
+    private CountDownLatch countDownLatch = new CountDownLatch(1);
+
+    public SetupFastMQ(String adr) {
+        this.ipAddress = adr;
+        //this.context = ZMQ.context(1);
+        //setupFastMQLocalSocket = context.socket(ZMQ.PULL);
+        //setupFastMQLocalSocket.bind(adr);
+    }
+
+    public void run() {
+        /*
+        while (!setupFastMQLocalSocket.recvStr().equals("SetupFastMQ")) {
+            //自旋，不过这里可以用Zk的分布式锁，其实性能呢~哈哈，过几天写一下ZK分布式锁
+        }*/
+        //分布式锁用来创建MQ
+        registSetupService();
+        while(!tryGetLock()){
+            //自旋
+        };
+        FastMQ.start();
+    }
+
+    /**
+     *
+     */
+    private void LoadBlance() {
+
+    }
+
+    /**
+     * 注册Setup服务，用来争抢锁以便开启MQ服务
+     */
+    private void registSetupService() {
+        ClusterManager.createMQServerNode(ipAddress);
+    }
+
+    /**
+     * 尝试争抢锁
+     */
+    private boolean tryGetLock() {
+        try {
+            for (; ; ) {
+                List<String> mqServer = ClusterManager.getMQServerNodes();
+                //当等于0时表示可以取到锁
+                int isGetTheLock = mqServer.indexOf(ipAddress);
+                //网络闪断
+                if (isGetTheLock < 0)
+                    throw new ZkNoNodeException("节点没有找到" + ipAddress);
+                if (isGetTheLock == 0) {
+                    return true;
+                }
+                String preNodeipAddress = mqServer.get(isGetTheLock - 1);
+                final CountDownLatch latch = new CountDownLatch(1);
+                final IZkDataListener iZkDataListener = new IZkDataListener() {
+                    @Override
+                    public void handleDataChange(String dataPath, byte[] data) throws Exception {
+
+                    }
+
+                    @Override
+                    public void handleDataDeleted(String dataPath) throws Exception {
+                        latch.countDown();
+                    }
+                };
+                ClusterManager.getZkClient().subscribeDataChanges("/MQServers/"+preNodeipAddress,iZkDataListener);
+                latch.await();
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
