9312c0f6b52a277c33a202ca7d60210e7e74c56e
Jason Chen <1538682075@qq.com>
2016-10-03 23:49:20 +0800

    add patition

--- src/main/java/MQ/Message/KeyMessage.java
+++ src/main/java/MQ/Message/KeyMessage.java
@@ -5,6 +5,12 @@ import java.util.Map;
 /**
  * Created by Jason Chen on 2016/10/2.
  */
+
+/**
+ * 消息的数据结构格式
+ * @param <K>
+ * @param <V>
+ */
 public class KeyMessage<K,V> implements Map.Entry<K,V> {
     private final K key;
     private V value;
---
+++ src/main/java/MQ/PatitionCollate.java
@@ -0,0 +1,12 @@
+package MQ;
+
+/**
+ * Created by Jason Chen on 2016/10/2.
+ */
+
+/**
+ * 分区策略
+ */
+public class PatitionCollate {
+
+}
