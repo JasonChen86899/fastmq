8c5889145518efdda02051d00ff2f1acf8ba2f15
Jason Chen <1538682075@qq.com>
2016-10-06 17:04:17 +0800

    Patition V2

--- src/main/java/MQ/patition/PatitionCollate.java
+++ src/main/java/MQ/patition/PatitionCollate.java
@@ -40,12 +40,12 @@ public class PatitionCollate {
     }
 
     /**
-     * 暂时想不出这个有什么用
+     * 注册 topic 事件，进行主题 分区数量的设定
      * @param topic_name
      * @param paitionNum
      * @throws IOException
      */
-    public static void subcriTopicEvent(final String topic_name, final int paitionNum) throws IOException {
+    public static void registTopicEvent(final String topic_name, final int paitionNum) throws IOException {
         zkClient.createEphemeral("/"+topic_name);
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         Hessian2Output hessian2Output = new Hessian2Output(byteArrayOutputStream);
@@ -77,14 +77,26 @@ public class PatitionCollate {
     }
 
     /**
-     * 负载均衡LoadBalance，对topic进行分组的本质原因,这里个kafka不一样，采用的是hashcode取模，（hash一致性算法这里没有用主要是应用场景的不同）
+     * 负载均衡LoadBalance，对topic进行分组的本质原因,这里个kafka不一样，采用的是hashcode取模，
+     * （hash一致性算法这里没有用主要是应用场景的不同）
+     *
+     * 这里是一个有别于kafka patition 的负载均衡和备份机制，也许就是一点点创新的地方吧
+     * 数据持久化方案则需要采取不同的方案，但是则需要数据的主从备份，甚至可以将数据持久化单独独立出来，这样性能就完全需要
+     * 闪存来保证
+     * -->单线程先从客户端拉数据，然后在各个broker上进行分发存储，保持数据的一致，然后各个broker从自己闪存中去数据，然后
+     * 创建队列，由消费者消费
+     * @param topic_name
      * @param keyMessage
+     * @return
+     * @throws IOException
      */
     public static String setTopicPatiton(String topic_name,KeyMessage<Object,Object> keyMessage) throws IOException {
-        byte[] bytes = zkClient.readData("//MessageData"+topic_name);
+        byte[] bytes = zkClient.readData("/MessageData"+topic_name);
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
         Hessian2Input hessian2Input = new Hessian2Input(byteArrayInputStream);
         List<String> ipList = (List)hessian2Input.readObject();
         return ipList.get(keyMessage.getKey().hashCode()%ipList.size()-1);
     }
+
+
 }
