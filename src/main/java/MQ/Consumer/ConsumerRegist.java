ead8b53b431f08b3396011820c62cee399cee030
JasonChen86899 <15952011990@163.com>
2016-11-25 20:34:17 +0800

    consumer regist v2

--- src/main/java/MQ/Consumer/ConsumerRegist.java
+++ src/main/java/MQ/Consumer/ConsumerRegist.java
@@ -1,12 +1,22 @@
 package MQ.Consumer;
 
+import MQ.Serialization.SerializationUtil;
+import com.github.zkclient.ZkClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.zeromq.ZMQ;
-import zmq.Address;
+
+import java.io.IOException;
+
+import java.util.HashSet;
 
 /**
  * Created by Jason Chen on 2016/11/23.
  */
+@Service
 public class ConsumerRegist extends Thread{
+    @Autowired
+    private ZkClient zkClient;
     private ZMQ.Context context;
     private ZMQ.Socket transfer;
     public ConsumerRegist(String tcpAddress){
@@ -17,9 +27,29 @@ public class ConsumerRegist extends Thread{
 
     public void run(){
         while (!Thread.currentThread().isInterrupted()){
-            String consumerIp = transfer.recvStr();
+            String group_consumerIp = transfer.recvStr();
+            String[] recStrArray = group_consumerIp.split("_");
+            String group = recStrArray[0];
+            String consumerIp = recStrArray[1];
             if (check_ip(consumerIp)){
-                
+                if(!zkClient.exists("/Consumer/Group"+group)) {
+                    zkClient.createEphemeral("/Consumer/Group" + group);
+                    HashSet<String> consumeripList = new HashSet<>();
+                    consumeripList.add(consumerIp);
+                    try {
+                        zkClient.writeData("/Consumer/Group" + group, SerializationUtil.serialize(consumeripList));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }else {
+                    try {
+                        HashSet<String> consumeripList = (HashSet<String>)SerializationUtil.deserialize(zkClient.readData("/Consumer/Group"));
+                        consumeripList.add(consumerIp);
+                        zkClient.writeData("/Consumer/Group" + group, SerializationUtil.serialize(consumeripList));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } 
+                }
             }else {
                 break;
             }
